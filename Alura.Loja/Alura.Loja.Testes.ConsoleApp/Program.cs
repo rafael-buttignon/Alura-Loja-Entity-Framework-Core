32240baa4eac3a4b57f79e179748a82451418b88
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-15 14:23:01 -0300

    Sincronizando o banco de dados com Migrations

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,109 +14,116 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-                var produtos = contexto.Produtos.ToList();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-
-                var novoProduto = new Produto()
-                {
-                    Nome = "Sabão em Pó",
-                    Categoria = "Limpeza",
-                    Preco = 12.99
-                };
-                contexto.Produtos.Add(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.Produtos.Remove(novoProduto);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                //contexto.SaveChanges();
-                var entry = contexto.Entry(novoProduto);
-                Console.WriteLine(entry.Entity.ToString() + " - " + entry.State);
-
-            }
         }
 
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
-        }
 
-        private static void AtualizarProduto()
-        {
-            GravarUsandoEntity();
-            RecuperarProdutos();
+        //private static void ChangeTrackerGerenciamentoDeEstados()
+        //{
 
-            using (var repo = new ProdutoDAOEntity())
-            {
-                Produto primeiro = repo.Produtos().First();
-                primeiro.Nome = "Rafael Royale - Editado";
-                repo.Atualizar(primeiro);
-            }
-            RecuperarProdutos();
-        }
+        //    using (var contexto = new LojaContext())
+        //    {
+        //        var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+        //        var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+        //        loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-        private static void ExcluirProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                foreach (var item in produtos)
-                {
-                    repo.Remover(item);
-                }
-            }
-        }
+        //        var produtos = contexto.Produtos.ToList();
 
-        private static void RecuperarProdutos()
-        {
-            using (var repo = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = repo.Produtos();
-                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
-                foreach (var item in produtos)
-                {
-                    Console.WriteLine(item.Nome);
-                }
-            }
-        }
+        //        ExibeEntries(contexto.ChangeTracker.Entries());
 
-        private static void GravarUsandoEntity()
-        {
-            Produto p = new Produto();
-            p.Nome = "Cassino Royale";
-            p.Categoria = "Filmes";
-            p.Preco = 19.89;
 
-            using (var repo = new ProdutoDAOEntity())
-            {
-                repo.Adicionar(p);
-            }
-        }
+        //        var novoProduto = new Produto()
+        //        {
+        //            Nome = "Sabão em Pó",
+        //            Categoria = "Limpeza",
+        //            PrecoUnitario = 12.99
+        //        };
+        //        contexto.Produtos.Add(novoProduto);
 
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
+        //        ExibeEntries(contexto.ChangeTracker.Entries());
 
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
+        //        contexto.Produtos.Remove(novoProduto);
+
+        //        ExibeEntries(contexto.ChangeTracker.Entries());
+
+        //        //contexto.SaveChanges();
+        //        var entry = contexto.Entry(novoProduto);
+        //        Console.WriteLine(entry.Entity.ToString() + " - " + entry.State);
+
+        //    }
+        //}
+
+        //private static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        //{
+        //    foreach (var e in entries)
+        //    {
+        //        Console.WriteLine(e.Entity.ToString() + " - " + e.State);
+        //    }
+        //}
+
+        //private static void AtualizarProduto()
+        //{
+        //    GravarUsandoEntity();
+        //    RecuperarProdutos();
+
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        Produto primeiro = repo.Produtos().First();
+        //        primeiro.Nome = "Rafael Royale - Editado";
+        //        repo.Atualizar(primeiro);
+        //    }
+        //    RecuperarProdutos();
+        //}
+
+        //private static void ExcluirProdutos()
+        //{
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        IList<Produto> produtos = repo.Produtos();
+        //        foreach (var item in produtos)
+        //        {
+        //            repo.Remover(item);
+        //        }
+        //    }
+        //}
+
+        //private static void RecuperarProdutos()
+        //{
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        IList<Produto> produtos = repo.Produtos();
+        //        Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
+        //        foreach (var item in produtos)
+        //        {
+        //            Console.WriteLine(item.Nome);
+        //        }
+        //    }
+        //}
+
+        //private static void GravarUsandoEntity()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Cassino Royale";
+        //    p.Categoria = "Filmes";
+        //    p.PrecoUnitario = 19.89;
+
+        //    using (var repo = new ProdutoDAOEntity())
+        //    {
+        //        repo.Adicionar(p);
+        //    }
+        //}
+
+        //private static void GravarUsandoAdoNet()
+        //{
+        //    Produto p = new Produto();
+        //    p.Nome = "Harry Potter e a Ordem da Fênix";
+        //    p.Categoria = "Livros";
+        //    p.PrecoUnitario = 19.89;
+
+        //    using (var repo = new ProdutoDAO())
+        //    {
+        //        repo.Adicionar(p);
+        //    }
+        //}
     }
 }
