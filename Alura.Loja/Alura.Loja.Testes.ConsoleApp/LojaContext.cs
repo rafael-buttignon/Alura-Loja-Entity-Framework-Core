3ad5fb1d5696b62e594dc984e32c03d80a5c051c
Rafael Buttignon <rafaaugustocontato@gmail.com>
2020-08-17 18:20:20 -0300

    Relacionamento Muitos para Muitos no Entity

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -7,7 +7,17 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Compra> Compras { get; set; }
+        public DbSet<Promocao> Promocoes { get; set; }
 
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<PromocaoProduto>()
+                .HasKey(pp => new { pp.PromocaoId, pp.ProdutoId });
+                base.OnModelCreating(modelBuilder);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
